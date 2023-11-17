76cfcbd0cf8cf2296d5a47ceb12bbc03b6bf11fe
mdiop23 <madina.diop20@gmail.com>
2023-11-17 12:32:58 -0500

    working

--- scripts/nationalpark.js
+++ scripts/nationalpark.js
@@ -1,17 +1,24 @@
 "use strict";
 
 // upon page loading, js knows to grab these two dropdowns
-window.onload= function(){
-  let searchByLocation= document.getElementById("searchByLocation") 
-  let searchByParkType= document.getElementById("searchByParkType")
 
-  searchByLocation.onchange= locationDropdownMenu;
+let searchByLocation = document.getElementById("searchByLocation");
+let searchByParkType = document.getElementById("searchByParkType");
+
+window.onload = function (){
+    console.log("Window onload event fired")
+    searchByLocation.onchange = locationDropdownMenu;
 }
-
+  
 // attaching locations array to states and adding options to dropdown
-function locationDropdownMenu(){
-    for(let states of locationsArray){
-        let options= new Option(states);
-        searchByLocation.appendChild(options)
-    }
+
+function locationDropdownMenu() {
+    //let defaultLocationOption = new Option("Select One", "");
+    //searchByLocation.appendChild(defaultLocationOption);
+    for (let location of locationsArray) {
+        let option = document.createElement("option");
+        option.value = location;
+        option.innerText = location;
+        searchByLocation.appendChild(option);}
 }
+
