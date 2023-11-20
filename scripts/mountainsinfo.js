d8bfb358366582e24ad05c944cdcb2e6b9548443
mdiop23 <madina.diop20@gmail.com>
2023-11-20 16:38:06 -0500

    added mountain images and done working through js

--- scripts/mountainsinfo.js
+++ scripts/mountainsinfo.js
@@ -1,32 +1,52 @@
 "use strict";
 
-
 // created variable that refers to the selectMountain ID in the mountains HTML to link them 
-const selectMountain = document.getElementById("selectMountain")
-
+const mountainSelectionDropdown = document.getElementById("selectMountain");
 
 // when the page loads the following info in the function init will be initialized
-window.onload = init; 
+window.onload = init;
 
-// function for what happens when info is intiated
+// function for what happens when info is initiated
 // calls the dropdown function
-function init(){
-    mountainDropdown();
-
+function init() {
+    displayMountainsDropdown();
+    mountainSelectionDropdown.onchange = dropdownSelection; // Fix function name here
 }
 
-
-// grabs info from mountainsArray in provided js file and displays the names as options
+// grabs info from mountainsArray in the provided js file and displays the names as options
 // cycles through each element in the array, recognizing each as a mountain
 // each mountain name becomes a clickable option with a value and also text so it can be displayed
 // adds each new upcoming option to the list of clickable options so that they're all displayed
+function displayMountainsDropdown() {
+    mountainsArray.forEach(mountain => {
+        let newOption = new Option(mountain.name);
+        mountainSelectionDropdown.appendChild(newOption);
+    });
+}
 
-function mountainDropdown(){
-    for (let mountain of mountainsArray) {
-        let option = document.createElement("option");
-        option.value = mountain.name;
-        option.text = mountain.name;
-        selectMountain.appendChild(option);
-      }
+// Function to handle dropdown selection
+function dropdownSelection() {
+    let mountainSelectedOption = mountainSelectionDropdown.value;
+    for (let j = 0; j < mountainsArray.length; j++) {
+        if (mountainSelectedOption == mountainsArray[j].name) {
+            newMtSelected(mountainsArray[j]);
+            break;
+        }
+    }
+}
+
+function newMtSelected(mountain) {
+    let nameOfMountain = document.getElementById("mountainName");
+    let elevationOfMountain = document.getElementById("elevation");
+    let mountainEffort = document.getElementById("effort");
+    let mountainInfo = document.getElementById("desc");
+    let mountainImage = document.getElementById("mountainImage");
+
+    mountainImage.src="images/" + mountain.img;
+    mountainInfo.innerText = mountain.desc;
+    nameOfMountain.innerText = "Name of Mountain: " + mountain.name;
+    elevationOfMountain.innerText = "Elevation: " + mountain.elevation;
+    mountainEffort.innerText = "Climbing Difficulty: " + mountain.effort; // Added colon for clarity
+
 }
 
